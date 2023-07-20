7c06c918a0890ca8bb63cded99bba38e054f8e42
Vadym <vdtrunov007@gmail.com>
2023-07-21 02:30:53 +0300

    new table in database

--- our_site/myapp/models.py
+++ our_site/myapp/models.py
@@ -1,6 +1,7 @@
+import django.contrib.auth.models
+from django.core.validators import MinLengthValidator, MaxLengthValidator, validate_email
 from django.db import models
 from django.utils.timezone import datetime
-
 
 class Note(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,3 +13,12 @@ class Note(models.Model):
         managed = True
         db_table = "notes"
 
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(unique=True, validators=[MinLengthValidator(4), MaxLengthValidator(32)])
+    password = models.TextField(validators=[MinLengthValidator(8), MaxLengthValidator(32)]) #hash найти!!!
+    email = models.CharField(validators=[validate_email])
+
+    class Meta:
+        managed = True
+        db_table = "users"
