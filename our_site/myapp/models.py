e5db08912b6129f784885e5a43e76700262846e1
Vadym <vdtrunov007@gmail.com>
2023-07-25 21:21:31 +0300

    changed user table

--- our_site/myapp/models.py
+++ our_site/myapp/models.py
@@ -5,20 +5,26 @@ from django.utils.timezone import datetime
 
 class Note(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(null=False, max_length=32)
-    content = models.TextField(null=False)
+    title = models.CharField(null=False, max_length=32, verbose_name="Назва")
+    content = models.TextField(null=False, verbose_name="Контент")
     created_as = models.DateTimeField(null=False, default=datetime.now)
 
     class Meta:
         managed = True
         db_table = "notes"
+        verbose_name = "Замітку"
+        verbose_name_plural = "Замітки"
+
+    def __str__(self):
+        return self.title
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.TextField(unique=True, validators=[MinLengthValidator(4), MaxLengthValidator(32)])
-    password = models.TextField(validators=[MinLengthValidator(8), MaxLengthValidator(32)]) #hash найти!!!
+    password = models.TextField(validators=[MinLengthValidator(8), MaxLengthValidator(32)])
     email = models.CharField(validators=[validate_email])
 
     class Meta:
         managed = True
         db_table = "users"
+
