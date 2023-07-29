99b7a8481f207efc74b914b62650cb40b89cf0c4
Vadym <vdtrunov007@gmail.com>
2023-07-29 18:33:06 +0300

    added links to admin panel

--- our_site/myapp/admin.py
+++ our_site/myapp/admin.py
@@ -4,14 +4,17 @@ from myapp.models import Note, User
 
 # Register your models here.
 
+
 class Note_admin(admin.ModelAdmin):
     search_fields = ("title", "content")
     list_display = ("id", "title", "content", "created_at")
+    list_display_links = ("title",)
 
 
 class User_admin(admin.ModelAdmin):
     search_fields = ("name", "email")
     list_display = ("id", "name", "email")
+    list_display_links = ("name",)
 
 
 
